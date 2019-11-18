9830bce005eaba20f2e7c2d68587f4c9960831fc
Del333ted <del333ted@protonmail.com>
2019-11-19 02:43:56 +0300

    .gitignore fix

--- dist/commands/help.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function setupHelp(bot) {
-    bot.command(['help', 'start'], ctx => {
-        ctx.replyWithHTML(ctx.i18n.t('hello'));
-    });
-}
-exports.setupHelp = setupHelp;
-//# sourceMappingURL=help.js.map
--- dist/helpers/bot.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const TelegrafBot = require('telegraf');
-exports.bot = new TelegrafBot(process.env.TOKEN);
-exports.bot.telegram.getMe().then(botInfo => {
-    const anybot = exports.bot;
-    anybot.options.username = botInfo.username;
-});
-//# sourceMappingURL=bot.js.map
