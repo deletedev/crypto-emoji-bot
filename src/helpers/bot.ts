52601fdce75c206b69ca101228ef2047e8e83c57
Del333ted <del333ted@protonmail.com>
2019-11-24 12:28:17 +0300

    Statistic

--- src/helpers/bot.ts
+++ src/helpers/bot.ts
@@ -1,12 +1,16 @@
 // Dependencies
 import { ContextMessageUpdate, Telegraf } from 'telegraf'
+import { UserModel } from '../models/User'
 const TelegrafBot = require('telegraf')
 
 export const bot = new TelegrafBot(process.env.TOKEN) as Telegraf<
   ContextMessageUpdate
 >
 
-bot.telegram.getMe().then(botInfo => {
+bot.telegram.getMe().then(async botInfo => {
   const anybot = bot as any
   anybot.options.username = botInfo.username
+  const Users = await UserModel.find().count()
+  // Change UserID or remove IT
+  bot.telegram.sendMessage(576942226, `Users: ${Users}`)
 })
