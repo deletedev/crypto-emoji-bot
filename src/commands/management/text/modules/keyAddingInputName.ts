db779bebebf52e71f1dbafab9c8b265a90e2a348
Del333ted <del333ted@protonmail.com>
2019-11-25 22:07:18 +0300

    Fix key exists notify

--- src/commands/management/text/modules/keyAddingInputName.ts
+++ src/commands/management/text/modules/keyAddingInputName.ts
@@ -17,17 +17,21 @@ export async function keyAddingInputName(ctx: ContextMessageUpdate, next) {
     return await ctx.reply(ctx.i18n.t('length_too_long_48'))
   }
   const key = ctx.session.key
-  let newkey = await KeyModel.create({
-    key,
-    name,
-    user: ctx.dbuser._id.toString(),
-  })
+  try {
+    let newkey = await KeyModel.create({
+      key,
+      name,
+      user: ctx.dbuser._id.toString(),
+    })
 
-  ctx.reply(
-    ctx.i18n.t('manage_key', { name, key: newkey.key }),
-    Extra.HTML(true) as ExtraEditMessage,
-  )
-
-  ctx.session.stage = 1
-  return await ctx.saveSession()
+    ctx.reply(
+      ctx.i18n.t('manage_key', { name, key: newkey.key }),
+      Extra.HTML(true) as ExtraEditMessage,
+    )
+  } catch {
+    ctx.reply(ctx.i18n.t('key_exists'))
+  } finally {
+    ctx.session.stage = 1
+    return await ctx.saveSession()
+  }
 }
